31d7a8a4ba7c1db302f5377a8af5f83d5cfae51c
Bucurenciu Bogdan Cristian <bucurenciu.cristian@gmail.com>
2021-10-10 21:11:29 +0300

    I have made some progress but it is not done.

---
+++ README.md
@@ -0,0 +1,13 @@
+###Parsezi fisierul si vei avea: 
+1. Produsul de neterminale
+2. Neterminale 
+3. Terminale
+4. Start
+5. directia
+
+###Produsul de neterminale
+Ajung sa am un array cu obiectele care trebuie.
+
+Oprirea programului trebuie sa fie cand string-ul final ajunge la 60 de caractere sau stringul contine doar terminale.
+
+La string-ul final trebuie replaced, nu adaugat(concat).
--- index.js
+++ index.js
@@ -8,101 +8,72 @@ const output = fs.readFileSync('data.txt', 'utf8')
 const productii = output[0];
 const neterminale = output[1];
 const terminale = output[2];
-const start = output[3];
+let start = output[3][0];
 const directia = output[4];
-const test = productii
-    .map(line => line
-        .split('->'));
-const split = test
-    .map(line => line
-        .map(x => x
-            .split('|')
-        )
-    );
+const separator = productii.map(line => line.split('->'));
+const split = separator.map(line => line.map(x => x.split('|')));
 
 let neTerminaleArray = [];
-
+/**
+ * In acest for de mai jos creez array-ul cu obiectele pe care le manipulez mai jos.
+ */
 for (let i = 0; i < productii.length; i++) {
     const Object = {};
     Object.name = split[i][0][0];
     Object.tablou = split[i][1];
     neTerminaleArray.push(Object);
 }
-
-let initialString = start[0];
-
-let final = initialString;
-let intermediar = '';
-
-const newVar = [...final];
-newVar.splice(0, 1, 'Salut');
-console.log(newVar)
-while (final.length < 60) {
-    console.log("iteratii-while: -------------------------------------------", final.length);
-    for (let i = 0; i < 60; i++) {
-        // console.log("final[i]: ", final[i]);
-        /**
-         * Intreb pentru fiecare caracter din string-ul final daca face parte din terminale
-         */
-        if (terminale.includes(final[i])) {
-
-            // console.log("P:", terminale);
-
-        } else if (neterminale.includes(final[i])) {
+console.log(neTerminaleArray);
+console.log(start);
+while (start.length < 60) {
+    let i = 0;
+    while (i < start.length) {
+        console.log("iteratia while-ului  al doilea este: ", i, " iar start[i]: ", start[i]);
+        if (neterminale.includes(start[i])) {
             /**
              * Gasesc index-ul elementului din neterminale
-             * @type {number}
              */
-            const el = neterminale.indexOf(final[i]);
+            const el = neterminale.indexOf(start[i]);
+            console.log("indexul din neTerminaleArray este: ", el);
             /**
              * Un numar random intre 0 si 1
-             * @type {number}
              */
             const random = Math.floor(Math.random() * 2);
             /**
              * Elementul urmator ce va fi inserat in string-ul final.
              */
             const nextElement = neTerminaleArray[el].tablou[random];
-            // console.log("urmatorul element", nextElement);
-
-            // spread operator pentru a face din string un array de charactere.
-            //Array.join pentru a readuce array-ul la un string.
-
-            final += nextElement;
-            final.splice();
-            console.log("finalul este:", final)
-
-
+            start[i];
+            //functie pentru substring de la poz 0 pana la poz neterminalului
+            const leftPart =  null;
+            //functie neterminal +1 pana la final sa imi ia.
+            const rightPart =  null;
+            // start.replace(start[i], nextElement);
+            start += nextElement;
         }
+        i++;
     }
 }
-console.log("Ba esti prost? ",final);
-
-/*let final = '';
-while (final.length < 60) {
-    for (let i = 0; i < 10; i++) {
-        final += i;
-    }
-    final += " ";
-    console.log(final);
-}*/
-// console.log("Dupa while: ",operation);
 
 
-// console.log("Neterminale: ", neterminale);
-// console.log("Terminale: ",terminale);
-// console.log("Start: ",start);
-// console.log(directia[0]);
+console.log("---------------!!-----------------------!!-------")
+console.log("Neterminale: ", neterminale);
+console.log("Terminale: ", terminale);
+console.log("Start: ", start);
 
-// console.log(JSON.stringify(split, null, 2));
-// .reduce((customers, line) => {
-//     customers[line[0]] = customers[line[0]]||[];
-//     customers[line[0]].push({
-//         name: line[1],
-//         price: line[2],
-//         quantity: line[3]
-//     })
-//
-//     return customers;
-//
-// }, {});
+console.log("directia :", directia);
+
+/*
+console.log(JSON.stringify(split, null, 2));
+.reduce((customers, line) => {
+    customers[line[0]] = customers[line[0]]||[];
+    customers[line[0]].push({
+        name: line[1],
+        price: line[2],
+        quantity: line[3]
+    })
+
+    return customers;
+
+}, {});
+*/
