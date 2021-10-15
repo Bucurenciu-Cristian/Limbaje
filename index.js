1a5b298dbb331764fd4cb8d29e3a5e94001da27d
Bucurenciu Bogdan Cristian <bucurenciu.cristian@gmail.com>
2021-10-15 20:45:28 +0300

    This Lab is done. It works great. Thanks to Markus and Teo.

--- index.js
+++ index.js
@@ -7,73 +7,64 @@ const output = fs.readFileSync('data.txt', 'utf8')
 
 const productii = output[0];
 const neterminale = output[1];
-const terminale = output[2];
-let start = output[3][0];
-const directia = output[4];
+const start = output[3][0] || 'E';
 const separator = productii.map(line => line.split('->'));
 const split = separator.map(line => line.map(x => x.split('|')));
-
-let neTerminaleArray = [];
 /**
- * In acest for de mai jos creez array-ul cu obiectele pe care le manipulez mai jos.
+ * Imi creez un array pe care il umplu cu obiecte pe care le manipulez mai tarziu
  */
+let neTerminaleArray = [];
 for (let i = 0; i < productii.length; i++) {
     const Object = {};
     Object.name = split[i][0][0];
     Object.tablou = split[i][1];
     neTerminaleArray.push(Object);
 }
-console.log(neTerminaleArray);
-console.log(start);
-while (start.length < 60) {
-    let i = 0;
-    while (i < start.length) {
-        console.log("iteratia while-ului  al doilea este: ", i, " iar start[i]: ", start[i]);
-        if (neterminale.includes(start[i])) {
+
+let final = start;
+console.log("inceputul este:", start);
+
+let contor = 0;
+/**
+ * Iterez peste sirul final pana cand ajung la n caractere , dar si cand sirul nu are neterminale, absolut deloc.
+ */
+while (final.length < 60 && contor !== final.length) {
+    /**
+     * Verific de fiecare data tot string-ul final.
+     */
+    for (let i = 0; i < final.length; i++) {
+        const caracter = final[i];
+        const gasesteNeterminala = neterminale.includes(caracter);
+        if (gasesteNeterminala) {
             /**
              * Gasesc index-ul elementului din neterminale
              */
-            const el = neterminale.indexOf(start[i]);
-            console.log("indexul din neTerminaleArray este: ", el);
+            const index = neterminale.indexOf(caracter);
+            /**
+             * Lungimea sirului array-ului cu care lucrez
+             */
+            const lungimea = neTerminaleArray[index].tablou.length;
             /**
              * Un numar random intre 0 si 1
              */
-            const random = Math.floor(Math.random() * 2);
+            const random = Math.floor(Math.random() * lungimea);
             /**
              * Elementul urmator ce va fi inserat in string-ul final.
              */
-            const nextElement = neTerminaleArray[el].tablou[random];
-            start[i];
-            //functie pentru substring de la poz 0 pana la poz neterminalului
-            const leftPart =  null;
-            //functie neterminal +1 pana la final sa imi ia.
-            const rightPart =  null;
-            // start.replace(start[i], nextElement);
-            start += nextElement;
+            const nextElement = neTerminaleArray[index].tablou[random];
+            console.log("Schimb elementul ",caracter, " cu ", nextElement);
+            /**
+             * Schimb neterminalul cu produsul neterminalului respectiv.
+             */
+            final = final.replace(caracter, nextElement);
+            console.log("Sirul actual este: ", final);
+            contor = 0;
+            i = final.length;
+        } else {
+            contor++;
         }
-        i++;
     }
 }
-
-
-console.log("---------------!!-----------------------!!-------")
-console.log("Neterminale: ", neterminale);
-console.log("Terminale: ", terminale);
-console.log("Start: ", start);
-
-console.log("directia :", directia);
-
-/*
-console.log(JSON.stringify(split, null, 2));
-.reduce((customers, line) => {
-    customers[line[0]] = customers[line[0]]||[];
-    customers[line[0]].push({
-        name: line[1],
-        price: line[2],
-        quantity: line[3]
-    })
-
-    return customers;
-
-}, {});
-*/
+console.log("-------------------------------------------------------------------")
+console.log("Lungimea sfarsitului este:", final.length);
+console.log("sfarsitul este:", final);
